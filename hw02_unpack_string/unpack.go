711632318bea385ac09585242f408c25fca1df00
b.ostrovskiy <b.ostrovskiy@omprussia.ru>
2022-04-28 02:40:25 +0300

    Task 2 additional solution

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,36 +9,69 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func isNextDigit(str []rune, currentPos int) bool {
+func isNextNotShieldedDigit(str []rune, currentPos int) bool {
 	if currentPos != len(str)-1 {
-		if unicode.IsDigit(str[currentPos+1]) {
+		if unicode.IsDigit(str[currentPos+1]) && string(str[currentPos-1]) != "\\" {
 			return true
 		}
 	}
 	return false
 }
 
+func isValidShielding(str []rune, currentPos int) bool {
+	if currentPos != len(str)-1 {
+		if !unicode.IsDigit(str[currentPos+1]) && string(str[currentPos+1]) != "\\" {
+			return false
+		}
+	}
+	return true
+}
+
 func Unpack(s string) (string, error) {
 	var sBuilder strings.Builder
 	str := []rune(s)
 	symbol := ""
+	shielding := false
 
 	for i := 0; i < len(str); i++ {
-		if !unicode.IsDigit(str[i]) {
+		switch {
+		case unicode.IsDigit(str[i]):
+			if i == 0 {
+				return "", ErrInvalidString
+			}
+			if next := isNextNotShieldedDigit(str, i); next {
+				return "", ErrInvalidString
+			}
+			if shielding {
+				symbol = string(str[i])
+				shielding = false
+			} else {
+				y, _ := strconv.Atoi(string(str[i]))
+				sBuilder.WriteString(strings.Repeat(symbol, y))
+				symbol = ""
+			}
+		case string(str[i]) == "\\":
+			if i == len(str)-1 {
+				return "", ErrInvalidString
+			}
+			if next := isValidShielding(str, i); !next {
+				return "", ErrInvalidString
+			}
+			if symbol != "" {
+				sBuilder.WriteString(symbol)
+				symbol = ""
+			}
+			if shielding {
+				symbol = string(str[i])
+				shielding = false
+			} else {
+				shielding = true
+			}
+		default:
 			if symbol != "" {
 				sBuilder.WriteString(symbol)
 			}
 			symbol = string(str[i])
-		} else {
-			if i == 0 {
-				return "", ErrInvalidString
-			}
-			if next := isNextDigit(str, i); next {
-				return "", ErrInvalidString
-			}
-			y, _ := strconv.Atoi(string(str[i]))
-			sBuilder.WriteString(strings.Repeat(symbol, y))
-			symbol = ""
 		}
 	}
 	sBuilder.WriteString(symbol)
