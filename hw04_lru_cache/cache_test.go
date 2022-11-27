bf7c73858464af5aac1dc50bfe14a8b5bd7d0cf1
b.ostrovskiy <b.ostrovskiy@omp.ru>
2022-11-28 00:36:18 +0300

    Task 4: LRU cache implementation

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,13 +50,94 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		t.Run("capacity overflow for queue with equal elements using", func(t *testing.T) {
+			c := NewCache(3)
+
+			c.Set("A", 1)
+			c.Set("B", 2)
+			c.Set("C", 3)
+			c.Set("D", 4)
+
+			element, exists := c.Get("B")
+			require.True(t, exists)
+			require.Equal(t, 2, element)
+
+			element, exists = c.Get("C")
+			require.True(t, exists)
+			require.Equal(t, 3, element)
+
+			element, exists = c.Get("D")
+			require.True(t, exists)
+			require.Equal(t, 4, element)
+
+			element, exists = c.Get("A")
+			require.False(t, exists)
+			require.Nil(t, element)
+		})
+
+		t.Run("capacity overflow for queue with not equal elements using", func(t *testing.T) {
+			c := NewCache(3)
+
+			c.Set("A", 1)
+			c.Set("B", 2)
+			c.Set("C", 3)
+
+			c.Get("A")
+			c.Get("B")
+			c.Get("A")
+			c.Set("D", 4)
+
+			element, exists := c.Get("A")
+			require.True(t, exists)
+			require.Equal(t, 1, element)
+
+			element, exists = c.Get("B")
+			require.True(t, exists)
+			require.Equal(t, 2, element)
+
+			element, exists = c.Get("D")
+			require.True(t, exists)
+			require.Equal(t, 4, element)
+
+			element, exists = c.Get("C")
+			require.False(t, exists)
+			require.Nil(t, element)
+		})
+	})
+
+	t.Run("clear cache", func(t *testing.T) {
+		c := NewCache(4)
+
+		c.Clear()
+		c.Set("A", 1)
+		c.Set("B", 2)
+
+		element, exists := c.Get("A")
+		require.Equal(t, true, exists)
+		require.Equal(t, 1, element)
+
+		element, exists = c.Get("B")
+		require.Equal(t, true, exists)
+		require.Equal(t, 2, element)
+
+		c.Clear()
+		element, exists = c.Get("A")
+		require.Equal(t, false, exists)
+		require.Equal(t, nil, element)
+
+		element, exists = c.Get("B")
+		require.Equal(t, false, exists)
+		require.Equal(t, nil, element)
+
+		c.Set("A", 1)
+		element, exists = c.Get("A")
+		require.Equal(t, true, exists)
+		require.Equal(t, 1, element)
 	})
 }
 
 func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
-
+	// t.Skip() // Remove me if task with asterisk completed.
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
@@ -72,6 +153,13 @@ func TestCacheMultithreading(t *testing.T) {
 		defer wg.Done()
 		for i := 0; i < 1_000_000; i++ {
 			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1_000_000; i++ {
+			c.Clear()
 		}
 	}()
 
