2d84a3c07d070a5c91ae74c981909beec9718048
b.ostrovskiy <b.ostrovskiy@omp.ru>
2023-01-04 01:06:12 +0300

    Task 6: pipeline execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,42 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := WithDoneStage(done, in)
+
+	for _, stage := range stages {
+		// Skip stage if it's nil
+		if stage != nil {
+			out = WithDoneStage(done, stage(out))
+		}
+	}
+	return out
+}
+
+func WithDoneStage(done In, in In) Out {
+	out := make(Bi)
+	go func() {
+		defer func() {
+			close(out)
+			for range in {
+			}
+		}()
+
+		for {
+			select {
+			case <-done:
+				return
+			case res, ok := <-in:
+				if !ok {
+					return
+				}
+
+				select {
+				case <-done:
+				case out <- res:
+				}
+			}
+		}
+	}()
+
+	return out
 }
