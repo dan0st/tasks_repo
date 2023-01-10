1526b8c28005aa1d85ce84673e682c64db3fce93
b.ostrovskiy <b.ostrovskiy@omp.ru>
2023-01-10 22:32:05 +0300

    Task 7: file copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,199 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
-var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
-)
+func sourceFileValidation(sourceFile string) error {
+	sourceFileInfo, err := os.Stat(sourceFile)
+	// Проверяем наличие указанного файла-источника
+	if err != nil {
+		if os.IsNotExist(err) {
+			return errors.New("source file is not exists")
+		}
+		return fmt.Errorf("getting source file info unexpected error: %w", err)
+	}
+
+	// Проверяем что указанный файл-источник - это не директория
+	if sourceFileInfo.IsDir() {
+		return errors.New("can't use source file - it's directory")
+	}
+
+	// Проверяем что указанный файл-источник - это регулярный файл
+	if !sourceFileInfo.Mode().IsRegular() {
+		return errors.New("can't use source file - it's not regular file")
+	}
+
+	return nil
+}
+
+func destinationFileValidation(destinationFile string) error {
+	f, err := os.Stat(destinationFile)
+	if err == nil {
+		if f.IsDir() {
+			return fmt.Errorf(
+				"directory with name \"%s\" already exist in destination",
+				destinationFile,
+			)
+		}
+
+		return nil
+	}
+
+	var pathError *fs.PathError
+	if ok := errors.As(err, &pathError); ok {
+		if errors.Is(pathError.Err, syscall.ENOTDIR) {
+			return fmt.Errorf("given destination \"%s\" is a directory", destinationFile)
+		}
+	} else {
+		return fmt.Errorf("unexpected error: %w", err)
+	}
+
+	return nil
+}
+
+func offsetValidation(sourceFile string, offset int64) error {
+	sourceFileStat, err := os.Stat(sourceFile)
+	if err != nil {
+		return fmt.Errorf("getting source file info unexpected error: %w", err)
+	}
+
+	if sourceFileStat.Size() < offset {
+		return errors.New("offset exceeds file size")
+	}
+
+	return nil
+}
+
+func validate(
+	sourceFile string,
+	destinationFile string,
+	offset int64,
+	limit int64,
+) error {
+	if sourceFile == "" {
+		return errors.New("source file not given")
+	}
+
+	if destinationFile == "" {
+		return errors.New("destination file not given")
+	}
+
+	if offset < 0 || limit < 0 {
+		return errors.New("offset and limit can't be less than 0")
+	}
+
+	// Валидируем файлы
+	err := sourceFileValidation(sourceFile)
+	if err != nil {
+		return fmt.Errorf("source file validation error: %w", err)
+	}
+
+	err = destinationFileValidation(destinationFile)
+	if err != nil {
+		return fmt.Errorf("destination file validation error: %w", err)
+	}
+
+	// Валидируем смещение
+	err = offsetValidation(sourceFile, offset)
+	if err != nil {
+		return fmt.Errorf("offset validation error: %w", err)
+	}
+
+	return nil
+}
+
+func getEndOfCopying(sourceFile string, offset int64, limit int64) (int64, error) {
+	sourceFileInfo, err := os.Stat(sourceFile)
+	if err != nil {
+		return 0, fmt.Errorf("getting source file info error: %w", err)
+	}
+
+	switch {
+	case limit == 0 && offset > 0:
+		return sourceFileInfo.Size() - offset, nil
+	case limit > 0 && offset == 0:
+		if limit > sourceFileInfo.Size() {
+			return sourceFileInfo.Size(), nil
+		}
+		return limit, nil
+	case limit > 0 && offset > 0:
+		restFile := sourceFileInfo.Size() - offset
+		if restFile-limit > 0 {
+			return limit, nil
+		}
+		return restFile, nil
+	}
+
+	return sourceFileInfo.Size(), nil
+}
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	err := validate(fromPath, toPath, offset, limit)
+	if err != nil {
+		log.Fatal(fmt.Errorf("validation: %w", err))
+	}
+
+	// limit рассматривается как количество копируемых байт либо от начала файла,
+	// либо от offset, если он задан
+	endOfCopying, err := getEndOfCopying(fromPath, offset, limit)
+	if err != nil {
+		log.Fatal(fmt.Errorf("end of copy calculation: %w", err))
+	}
+
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("can't open source file while for copying: %w", err)
+	}
+	defer func() {
+		err := fromFile.Close()
+		if err != nil {
+			fmt.Printf("closing source file error: %v", err)
+		}
+	}()
+
+	if offset > 0 {
+		_, err := fromFile.Seek(offset, 0)
+		if err != nil {
+			return fmt.Errorf("do offset in file error: %w", err)
+		}
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("can't create destination file for copying: %w", err)
+	}
+	defer func() {
+		err := toFile.Close()
+		if err != nil {
+			fmt.Printf("closing destination file error: %v", err)
+		}
+	}()
+
+	bar := pb.Full.Start64(endOfCopying)
+	barFromFileProxyReader := bar.NewProxyReader(fromFile)
+
+	_, err = io.CopyN(toFile, barFromFileProxyReader, endOfCopying)
+	if err != nil {
+		return fmt.Errorf("error while copying: %w", err)
+	}
+	bar.Finish()
+
+	start := time.Now()
+	fmt.Println("Start sync file")
+	err = toFile.Sync()
+	if err != nil {
+		return fmt.Errorf("file sync after copying error: %w", err)
+	}
+	fmt.Printf("File sync finished in %v \n", time.Since(start))
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,314 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	sourceTestFile             = "testdata/input.txt"
+	minValidTestFileSize int64 = 6000
+	maxValidTestFileSize int64 = 7000
+)
+
+func validateTestSourceFile(t *testing.T) {
+	t.Helper()
+	require.FileExists(t, sourceTestFile, "test file not exist")
+
+	sourceTestFileStat, err := os.Stat(sourceTestFile)
+	require.NoError(t, err, "cant get test source file stat")
+
+	require.GreaterOrEqualf(
+		t,
+		sourceTestFileStat.Size(),
+		minValidTestFileSize,
+		"invalid test source file, it size should be greater or equal %d",
+		minValidTestFileSize,
+	)
+	require.LessOrEqual(
+		t,
+		sourceTestFileStat.Size(),
+		maxValidTestFileSize,
+		"invalid test source file, it size should be less or equal %d",
+		maxValidTestFileSize,
+	)
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	validateTestSourceFile(t)
+
+	// Данные кейсы не отлавливаются на этапе валидации так как для это необходима
+	// дополнительная более сложная обработка передаваемого пути назначения,
+	// поэтому они будут получены непосредственно при инициализации копирования.
+	// Вариации позитивных кейсов копирования проверяются в test.sh.
+
+	// Корректно отработает только на UNIX-подобных системах.
+	t.Run("destination not created before and it's directory", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "test/", 0, 0)
+
+		// Ожидаем ошибку, что переданный destination - это директория.
+		require.ErrorIs(t, err, syscall.EISDIR)
+	})
+
+	// Корректно отработает только на UNIX-подобных системах.
+	t.Run("non-existent destination path", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "test_dir/test", 0, 0)
+
+		// Ожидаем ошибку, что переданный destination не существует.
+		require.ErrorIs(t, err, syscall.ENOENT)
+	})
+}
+
+func Test_validate(t *testing.T) {
+	validateTestSourceFile(t)
+
+	sourceTestFileStat, err := os.Stat(sourceTestFile)
+	require.NoError(t, err, "cant get test source file stat")
+
+	type args struct {
+		sourceFile      string
+		destinationFile string
+		offset          int64
+		limit           int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success - valid fields",
+			args: args{
+				sourceFile:      sourceTestFile,
+				destinationFile: "resFile",
+				offset:          0,
+				limit:           0,
+			},
+			wantErr: false,
+		},
+		{
+			name: "fail - empty source",
+			args: args{
+				sourceFile:      "",
+				destinationFile: "resFile",
+				offset:          0,
+				limit:           0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "fail - empty dest",
+			args: args{
+				sourceFile:      sourceTestFile,
+				destinationFile: "",
+				offset:          0,
+				limit:           0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "fail - negative limit",
+			args: args{
+				sourceFile:      sourceTestFile,
+				destinationFile: "resFile",
+				offset:          0,
+				limit:           -1,
+			},
+			wantErr: true,
+		},
+		{
+			name: "fail - negative offset",
+			args: args{
+				sourceFile:      sourceTestFile,
+				destinationFile: "resFile",
+				offset:          -1,
+				limit:           0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "fail - offset greater than file size",
+			args: args{
+				sourceFile:      sourceTestFile,
+				destinationFile: "resFile",
+				offset:          sourceTestFileStat.Size() + 1000,
+				limit:           0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "fail - non-existent source file",
+			args: args{
+				sourceFile:      "nonExistentFile",
+				destinationFile: "resFile",
+				offset:          sourceTestFileStat.Size() + 1000,
+				limit:           0,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validate(
+				tt.args.sourceFile,
+				tt.args.destinationFile,
+				tt.args.offset, tt.args.limit,
+			); (err != nil) != tt.wantErr {
+				t.Errorf("validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_getEndOfCopying(t *testing.T) {
+	validateTestSourceFile(t)
+
+	sourceTestFileStat, err := os.Stat(sourceTestFile)
+	require.NoError(t, err, "cant get test source file stat")
+
+	type args struct {
+		sourceFile string
+		offset     int64
+		limit      int64
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    int64
+		wantErr bool
+	}{
+		{
+			name: "success - limit and offset not given",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     0,
+				limit:      0,
+			},
+			want:    sourceTestFileStat.Size(),
+			wantErr: false,
+		},
+		{
+			name: "success - only offset given",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     1000,
+				limit:      0,
+			},
+			want:    sourceTestFileStat.Size() - 1000,
+			wantErr: false,
+		},
+		{
+			name: "success - only limit given and limit greater than source file",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     0,
+				limit:      maxValidTestFileSize + 1000,
+			},
+			want:    sourceTestFileStat.Size(),
+			wantErr: false,
+		},
+		{
+			name: "success - only limit given and limit less than source file",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     0,
+				limit:      1000,
+			},
+			want:    1000,
+			wantErr: false,
+		},
+		{
+			name: "success - limit and offset given but limit less file after offset",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     100,
+				limit:      1000,
+			},
+			want:    1000,
+			wantErr: false,
+		},
+		{
+			name: "success - limit and offset given but limit greater file after offset",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     5500,
+				limit:      2000,
+			},
+			want:    sourceTestFileStat.Size() - 5500,
+			wantErr: false,
+		},
+		{
+			name: "fail - getting source file info",
+			args: args{
+				sourceFile: "notExistedFile",
+				offset:     0,
+				limit:      0,
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getEndOfCopying(tt.args.sourceFile, tt.args.offset, tt.args.limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getEndOfCopying() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("getEndOfCopying() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_offsetValidation(t *testing.T) {
+	validateTestSourceFile(t)
+
+	type args struct {
+		sourceFile string
+		offset     int64
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "success - given valid file path and offset",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     1000,
+			},
+			wantErr: false,
+		},
+		{
+			name: "fail - getting source file info",
+			args: args{
+				sourceFile: "notExistedFile",
+				offset:     0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "fail - offset greater than given file",
+			args: args{
+				sourceFile: sourceTestFile,
+				offset:     maxValidTestFileSize + 1000,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := offsetValidation(tt.args.sourceFile, tt.args.offset); (err != nil) != tt.wantErr {
+				t.Errorf("offsetValidation() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
