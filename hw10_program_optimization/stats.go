f3efe25cbc4ea66990268cc46f5ee9cddc68a9bd
b.ostrovskiy <b.ostrovskiy@omp.ru>
2023-01-17 03:35:00 +0300

    Task 10: program optimization
    Optimization results in benchstat:
    name         old time/op    new time/op    delta
    GetDomain-8    8.72ms ± 8%    9.90ms ± 2%  +13.53%
    
    name         old alloc/op   new alloc/op   delta
    GetDomain-8    2.76MB ±12%    0.11MB ± 6%  -96.17%
    
    name         old allocs/op  new allocs/op  delta
    GetDomain-8     26.8k ±12%      5.6k ± 6%  -79.15%

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,14 +1,18 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
+	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"regexp"
 	"strings"
+
+	"github.com/mailru/easyjson"
 )
 
+var ErrEmptyDomain = errors.New("empty domain")
+
+//easyjson:json
 type User struct {
 	ID       int
 	Name     string
@@ -22,6 +26,10 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+	if domain == "" {
+		return nil, ErrEmptyDomain
+	}
+
 	u, err := getUsers(r)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
@@ -32,35 +40,29 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
+	scanner := bufio.NewScanner(r)
+	var i int
 
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
+	for scanner.Scan() {
 		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
+		if err = easyjson.Unmarshal(scanner.Bytes(), &user); err != nil {
 			return
 		}
+
 		result[i] = user
+		i++
 	}
-	return
+
+	return result, scanner.Err()
 }
 
 func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
+	domain = strings.ToLower(domain)
 
 	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if strings.Contains(user.Email, "."+domain) {
+			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
 		}
 	}
 	return result, nil
